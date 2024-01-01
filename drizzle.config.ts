30f47b491a9d897bd50ebeb921745436ec5ed96e
Bhawesh <bhawesh.v@indecab.com>
2024-01-01 19:59:35 +0530

    init(packages): base packages installed and orm config

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+if (!process.env.DATABASE_URL) {
+  console.log('DATABASE_URL is not defined');
+  throw new Error('DATABASE_URL is not defined');
+}
+
+export default {
+  schema: './src/lib/supabase/schema.ts',
+  out: './migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL || '',
+  },
+} satisfies Config;
