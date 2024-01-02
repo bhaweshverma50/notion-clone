a4936d9f609d0a22b85e6da9b7d3d1b562f46816
Bhawesh <53930628+bhaweshverma50@users.noreply.github.com>
2024-01-02 19:37:55 +0530

    init: initialised eslint, prettier and husky config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
-/** @type {import("prettier").Config} */
-const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-  printWidth: 120,
-  plugins: ['prettier-plugin-tailwindcss']
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: 'auto',
+    arrowParens: 'avoid',
+    trailingComma: 'es5',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
 };
-
-module.exports = config;
